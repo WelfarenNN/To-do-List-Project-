a4d97871dc4935085e4bc4d7ee47e46eb1d9069f
26LP89151895 <26lp89151895@G401-CC0213.local>
2026-07-29 12:01:24 +0800

    todo...

--- src/app/page.js
+++ src/app/page.js
@@ -1,66 +1,37 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import { useState } from "react";
 export default function Home() {
+  const [inputValue, setInputValue] = useState("");
+  const HandleInputValue = () => {
+    const value = e.target.value
+    setInputValue(value)
+  }
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className={styles.primaryContainer}>
+      <div className={styles.container}>
+        <div className={styles.piContainer}>
+          <div className={styles.innerContainer}>
+            <span className={styles.todolist}>To-Do list</span>
+            <div className={styles.addtaskContainer}>
+              <span className={styles.addtask}>Add a new task...</span>
+              <span>Add</span>
+            </div>
+            <div className={styles.buttons}>
+              <button className={styles.buttonAll}>All</button>
+              <button className={styles.buttonActive}>Active</button>
+              <button className={styles.buttonComplete}>Completed</button>
+            </div>
+          </div>
+          <span className={styles.piFooter}>No tasks yet. Add one above!</span>
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className={styles.containerFooter}>
+          <span className={styles.CFT1}>Powered by</span>
+          <span className={styles.CFT2}>Pinecone academy</span>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
